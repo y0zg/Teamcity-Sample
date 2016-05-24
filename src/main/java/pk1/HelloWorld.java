cb82d40f5b6e51810f205f89db4789cc3eda95b3
Julia-Alexandrova <Julia-Alexandrova@users.noreply.github.com>
2016-05-24 18:45:49 +0300

    Update HelloWorld.java

--- src/main/java/pk1/HelloWorld.java
+++ src/main/java/pk1/HelloWorld.java
@@ -6,7 +6,8 @@ private String name;
      public static void main(String [] args) {
      System.out.println(new HelloWorld("Test").sayHello());
          System.out.println("Bye");
- //      more  changes to testda;gjs;agjhas
+ //      more  changes to test
+ //df;glkh;gdshj;
      }
   public HelloWorld(String name) {
       this.name = name;
